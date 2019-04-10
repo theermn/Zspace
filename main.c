c36a2e12c6826accee1bb29d766c70f2eea7fd7f
theermn <49471442+theermn@users.noreply.github.com>
2019-04-10 12:42:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
-/*
- */
 
 #include <avr/io.h>
+#include<util/delay.h>
+
 
 int main(void)
 {
-
-    // Insert code
+    DDRD=1;
 
     while(1)
-    ;
+    {
+        PORTD=1;
+        _delay_ms(2000);
+        PORTD=0;
+        _delay_ms(2000);
+    }
 
     return 0;
-}
+    }
